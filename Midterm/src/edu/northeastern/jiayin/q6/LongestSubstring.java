800eb39b65fc0141251eeab338d38f33ea4dc9f0
jsong0727 <songjiayin082@gmail.com>
2022-03-26 17:14:18 -0700

    Midterm Jiayin

--- Midterm/src/edu/northeastern/jiayin/q6/LongestSubstring.java
+++ Midterm/src/edu/northeastern/jiayin/q6/LongestSubstring.java
@@ -3,13 +3,13 @@ package edu.northeastern.jiayin.q6;
 import java.util.HashMap;
 
 public class LongestSubstring {
-    
+
     public int longestSubstring(String string) {
         if (string.length() == 0) {
             return 0;
         }
 
-        HashMap<Character, Integer> hashmap = new HashMap<Character, Integer>();
+        HashMap<Character, Integer> hashmap = new HashMap<>();
         int longest = 0;
         for (int i = 0, j = 0; i < string.length(); ++i) {
             if (hashmap.containsKey(string.charAt(i))) {
